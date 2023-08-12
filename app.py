85cb8dd7d248eee3c2113d9d1f43dcc56efc40af
saeedm31 <saeedm31@gmail.com>
2023-08-12 21:05:57 +0100

    edit the design

--- app.py
+++ app.py
@@ -4,45 +4,61 @@ from dash.dependencies import Input, Output, State
 import openai
 import os
 
-# import configparser
+import configparser
+
+# Load API key from config.ini
 # config = configparser.ConfigParser()
-# config.read('config.ini')  # Replace 'config.ini' with the actual path to your config file
+# config.read('config.ini')
 # api_key = config.get('OpenAI', 'api_key')
-
-
 openai.api_key = os.environ["api_key"]
-
 
 app = dash.Dash(__name__)
 
 server = app.server
 
-app.layout = html.Div([
-    html.H1("Dictionary"),
-    dcc.Textarea(id="input-box", value="", rows=10, cols=50, maxLength=200),
-    html.Button("Translate it", id="translate-button", n_clicks=0),
-    html.H2("Here are the translation:"),
-    html.Div(id="translation-output"),
-    html.Button("Clear", id="clear-button"),
-])
-
+app.layout = html.Div(
+    children=[
+        html.H1("Portuguese Translate and Dictionary (pordic V.1)", style={"text-align": "center"}),
+        html.Div([
+            html.H3("Translate text from any language to Portuguese (Portugal version) or from Portuguese to \
+                    English with the power of CHATGPT."),
+            dcc.Textarea(id="input-box", value="", rows=5, cols=50, maxLength=200),
+            html.Div([
+                html.Div([
+                    html.Button("Translate", id="translate-button", n_clicks=0, className="btn-translate"),
+                ], className="translate-button-container"),
+                html.Div(id="translation-output", className="translation-box"),
+            ], className="button-group"),
+            html.Div([html.Br(), html.Br()]),  # Add space between the button group and the Clear button
+            html.Div([
+                html.Button("Clear", id="clear-button", n_clicks=0, className="btn-clear"),
+            ], className="clear-button-container"),
+        ], className="translation-container"),
+    ],
+    className="main-container",
+    style={"backgroundColor": "lightgreen", "padding": "20px"}
+)
 
 @app.callback(
     Output("translation-output", "children"),
     Input("translate-button", "n_clicks"),
     State("input-box", "value")
 )
-def dic_response(n_clicks, input_text):
+def translate_text(n_clicks, input_text):
     if n_clicks > 0 and input_text:
         response = openai.Completion.create(
             engine="text-davinci-003",
-            prompt=f"Translate this in Portuguese from Portugal, DO NOT USE Portuguese from Brazil,\
-                  only use Portuguese from Portugal, only the translation with no explnatio, here is the text: {input_text}",
+            prompt=f"Translate this Text into Portuguese from Portugal, but please exclude Brazilian Portuguese. If the Text \
+                 is in Portuguese, please translate it to English, here is the Text: {input_text}",
             temperature=0,
-            max_tokens=400
+            max_tokens=400,
+            api_key=api_key
         )
         translation = response.choices[0].text.strip()
-        return translation
+        return html.Div([
+            dcc.Markdown(f"{translation}"),
+            dcc.Markdown(f"**Translation:**")
+        ])
 
 @app.callback(
     Output("input-box", "value"),
@@ -51,7 +67,6 @@ def dic_response(n_clicks, input_text):
 def clear_input(n_clicks):
     if n_clicks is not None and n_clicks > 0:
         return ""
-
 
 if __name__ == "__main__":
     app.run(debug=True, port=int(os.environ.get("PORT", 5000)), host='0.0.0.0')
